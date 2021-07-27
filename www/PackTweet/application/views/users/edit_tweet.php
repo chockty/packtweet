5c6f1976eb1de6b49c67f4cadc46d8b1bb8ee6da
照井拓朗 <>
2021-07-27 20:07:25 +0900

    編集画面作成

---
+++ www/PackTweet/application/views/users/edit_tweet.php
@@ -0,0 +1,15 @@
+    <div class>
+      <div class="container">
+        <div class="title">ツイート編集</div>
+        <?= form_open('tweets/' . $tweet['tweet_id'] . '/edit'); ?>
+          <div class="form-group">
+            <?= form_textarea(['name' => 'content',  'class' => 'form-control', 'value' => $tweet['content'], 'cols' => '50', 'rows' => '10']); ?>
+            <span class="help-block"><?= form_error('content'); ?></span>
+          </div>
+          <?= form_button(['type' => 'submit', 'class' => 'btn btn-success pull-right', 'content' => "更新する", 'value' =>  set_value('content')]) ?>
+        <?= form_close(); ?>
+      </div>
+    </div>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/moment.js/2.0.0/moment.min.js"></script>
+  </body>
+</html>
