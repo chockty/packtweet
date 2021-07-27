45eb36fdeaaf48d8d82925dd8ccf8a143c529872
照井拓朗 <>
2021-07-27 13:21:24 +0900

    リファクタリング

--- www/PackTweet/application/views/users/create_tweet.php
+++ www/PackTweet/application/views/users/create_tweet.php
@@ -1,23 +1,12 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <base href="<?php echo base_url(); ?>">
-    <link href="application/public/css/app.css" rel="stylesheet">
-    <title></title>
-  </head>
-  <body>
     <div class>
       <div class="container">
         <div class="title">ツイート投稿</div>
-        <?= form_open('tweet/create', array('method'=>'POST')); ?>
+        <?= form_open('tweet/create', ['method'=>'POST']); ?>
           <div class="form-group">
-            <?= form_textarea(array('name' => 'content',  'class' => 'form-control', 'placeholder' => 'いまどうしてる？', 'cols' => '50', 'rows' => '10')); ?>
+            <?= form_textarea(['name' => 'content',  'class' => 'form-control', 'placeholder' => 'いまどうしてる？', 'cols' => '50', 'rows' => '10']); ?>
             <span class="help-block"><?= form_error('content'); ?></span>
           </div>
-          <?php echo form_button(array('type' => 'submit', 'class' => 'btn btn-success pull-right', 'content' => "ツイートする")) ?>
-        </form>
+          <?= form_button(['type' => 'submit', 'class' => 'btn btn-success pull-right', 'content' => "ツイートする", 'value' =>  set_value('content')]) ?>
         <?= form_close(); ?>
       </div>
     </div>
