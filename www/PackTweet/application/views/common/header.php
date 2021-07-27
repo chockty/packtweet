f9928b3c7d5454ff0ab09e6520e91ef699834232
chockty <75976150+chockty@users.noreply.github.com>
2021-07-27 17:08:17 +0900

    サイドメニュー用のcssを読み込む処理を追加

--- www/PackTweet/application/views/common/header.php
+++ www/PackTweet/application/views/common/header.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <base href="<?= base_url(); ?>">
     <link href="application/public/css/app.css" rel="stylesheet">
+		<link href="application/public/css/sidebar.css" rel="stylesheet" >
+		<link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet" >
     <title></title>
   </head>
   <body>
