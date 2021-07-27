cb7c39e6216361103d96251e6c37912024a3a90c
chockty <75976150+chockty@users.noreply.github.com>
2021-07-27 17:06:49 +0900

    サイドメニューを作成

---
+++ www/PackTweet/application/views/common/sidebar.php
@@ -0,0 +1,41 @@
+<div class="sidebar">
+	<div class="twitter-icon">
+		<i class="fab fa-twitter fa-3x"></i>
+	</div>
+	<div class="sidebar-menu">
+		<ul>
+			<li>
+				<a href="#">
+					<i class="fas fa-home"></i>&nbsp;&nbsp;ホーム
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<i class="fas fa-hashtag"></i>&nbsp;&nbsp;話題を検索
+				</a>
+			</li>
+			<li>
+				<a href="#" >
+					<i class="far fa-bell"></i>&nbsp;&nbsp;通知
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<i class="far fa-envelope"></i>&nbsp;&nbsp;メッセージ
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<i class="far fa-user"></i>&nbsp;&nbsp;プロフィール
+				</a>
+			</li>
+		</ul>
+	</div>
+	<div class="link-to-post">
+		<button class="btn btn-gradient" style=>
+			<a href="/tweets/create">ツイートする</a>
+		</button>
+	</div>
+</div>
+</body>
+</html>
