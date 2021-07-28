6c8a600a59eefcbca7de3d9d978b5c1a433d929f
照井拓朗 <>
2021-07-28 17:22:58 +0900

    ツイートの並び順指定

--- www/PackTweet/application/models/Tweet_model.php
+++ www/PackTweet/application/models/Tweet_model.php
@@ -74,6 +74,7 @@ class Tweet_model extends CI_Model
         $this->db->select('name, content, tweets.created_at, tweets.id AS tweet_id');
         $this->db->where('user_id', $userId);
         $this->db->where('tweets.deleted_at', NULL);
+        $this->db->order_by('tweets.created_at', 'DESC');
         $this->db->join('users', 'users.id = tweets.user_id', 'left');
         $query = $this->db->get('tweets');
         return $query->result_array();
