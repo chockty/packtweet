09a6c75d8984295ee3b5dee4cfff1bf0c5e7653a
chockty <75976150+chockty@users.noreply.github.com>
2021-07-28 12:03:56 +0900

    headerに必要なlinkタグを追加

--- www/PackTweet/application/views/common/header.php
+++ www/PackTweet/application/views/common/header.php
@@ -7,6 +7,8 @@
     <link href="application/public/css/app.css" rel="stylesheet">
 		<link href="application/public/css/sidebar.css" rel="stylesheet" >
 		<link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet" >
+		<link rel="stylesheet" href="application/public/css/user.css">
+		<link rel="stylesheet" href="application/public/css/nav-fix.css">
+		<link rel="stylesheet" href="application/public/css/index.css">
     <title></title>
   </head>
-  <body>
