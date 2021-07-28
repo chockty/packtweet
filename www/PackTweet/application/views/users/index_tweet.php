0ad0e280d0bd9584b884438348e384ba6a32bf60
chockty <75976150+chockty@users.noreply.github.com>
2021-07-28 15:21:31 +0900

    ツイート投稿者の名前が表示されるよう修正

--- www/PackTweet/application/views/users/index_tweet.php
+++ www/PackTweet/application/views/users/index_tweet.php
@@ -2,13 +2,13 @@
   <div class="content-wrapper table-responsive">
     <div class="tweet-boxes">
         <? foreach ($tweets as $tweet) : ?>
-				<a class="tweet-link" href="<?= site_url('tweets/'. $tweet['id'] . '/show') ?>">
+        <a href="<?= site_url('tweets/'. $tweet['id'] . '/show') ?>">
           <div class="tweet-box">
-            <div class="user-info"><a href="#"><?= $tweet['user_id'] ?></a></div>
+            <div class="user-info"><?= $tweet['name'] ?></div>
             <div class="tweet-index-content"><?= $tweet['content'] ?></div>
             <div class="tweet-index-created-at"><?= $tweet['created_at'] ?></div>
           </div>
-				</a>
+        </a>
         <? endforeach; ?>
     </div>
   </div>
