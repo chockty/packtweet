b2d25ea0327b6f64224d8466f050620b475c18c1
chockty <75976150+chockty@users.noreply.github.com>
2021-07-29 12:21:48 +0900

    検索結果が0件の時の表示を追加

--- www/PackTweet/application/views/users/index_tweet.php
+++ www/PackTweet/application/views/users/index_tweet.php
@@ -1,20 +1,26 @@
 <div class="main-wrap">
   <div class="content-wrapper table-responsive">
     <div class="tweet-boxes">
-      <? foreach ($tweets as $tweet) : ?>
-        <a href="<?= site_url('tweets/'. $tweet['tweet_id'] ) ?>">
-          <div class="tweet-box">
-            <div class="user-info"><?= $tweet['name'] ?></div>
-            <div class="tweet-index-content"><?= $tweet['content'] ?></div>
-            <div class="tweet-index-created-at"><?= $tweet['created_at'] ?></div>
-          </div>
-        </a>
-      <? endforeach; ?>
+      <? if (count($tweets) === 0): ?>
+        <div class="no-result-alert">
+          <?= $search_word ?>の検索結果はありませんでした。
+        </div>
+      <? else: ?>
+        <? foreach ($tweets as $tweet) : ?>
+          <a href="<?= site_url('tweets/'. $tweet['tweet_id'] ) ?>">
+            <div class="tweet-box">
+              <div class="user-info"><?= $tweet['name'] ?></div>
+              <div class="tweet-index-content"><?= $tweet['content'] ?></div>
+              <div class="tweet-index-created-at"><?= $tweet['created_at'] ?></div>
+            </div>
+          </a>
+        <? endforeach; ?>
+      <? endif ?>
     </div>
       <div class="search-box">
-        <?= form_open('/tweets', ['method' => 'GET']) ?>
+        <?= form_open('tweets', ['method' => 'GET']) ?>
           <?= form_button(['name' => 'submit', 'type' => 'submit', 'content' => '<i class="fas fa-search fa-2x"></i>']) ?>
-          <?= form_input(['name' => 'search_word', 'class' => 'input-box']) ?>
+          <?= form_input(['name' => 'search_word', 'class' => 'input-box', 'value' => $search_word]) ?>
         <?= form_close() ?>
       </div>
     </div>
