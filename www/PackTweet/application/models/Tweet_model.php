a2ab41af98f03cfb9485024fd72d528543124645
照井拓朗 <>
2021-07-29 19:16:52 +0900

    リツイート処理実装

--- www/PackTweet/application/models/Tweet_model.php
+++ www/PackTweet/application/models/Tweet_model.php
@@ -45,9 +45,12 @@ class Tweet_model extends CI_Model
 
     public function getByTweetId($tweetId)
     {
-        $this->db->select('name, content, tweets.created_at, tweets.id AS tweet_id');
+        $getRetweets = '(select tweet_id, count(*) AS retweet_count from retweet_tweet left outer join retweets on retweets.id = retweet_tweet.retweet_id group by tweet_id) AS retweet_counts';
+
+        $this->db->select('name, content, tweets.created_at, tweets.id AS tweet_id, retweet_count');
         $this->db->where('tweets.id', $tweetId);
         $this->db->join('users', 'users.id = tweets.user_id', 'left');
+        $this->db->join($getRetweets, 'retweet_counts.tweet_id = tweets.id', 'left');
         $query = $this->db->get('tweets');
         return $query->row_array();
     }
@@ -85,14 +88,6 @@ class Tweet_model extends CI_Model
 
     public function getByUserId($userId)
     {
-        // $this->db->select('name, content, tweets.created_at, tweets.id AS tweet_id');
-        // $this->db->where('user_id', $userId);
-        // $this->db->where('tweets.deleted_at', NULL);
-        // $this->db->order_by('tweets.created_at', 'DESC');
-        // $this->db->join('users', 'users.id = tweets.user_id', 'left');
-        // $query = $this->db->get('tweets');
-        // return $query->result_array();
-
         $getRetweets = '(select tweet_id, retweet_user_id, name AS retweet_user_name, retweet_time from (select tweet_id, user_id AS retweet_user_id,  created_at AS retweet_time from retweet_tweet left outer join retweets on retweets.id = retweet_tweet.retweet_id) AS get_retweets_user left outer join users on users.id = get_retweets_user.retweet_user_id) AS get_retweets';
 
         $tweets = $this->db->select('name, retweet AS retweet_user_name, content, tweets.created_at, tweets.id AS tweet_id, retweet AS retweet_time, tweets.created_at AS order_time')
@@ -112,5 +107,30 @@ class Tweet_model extends CI_Model
                              ->get_compiled_select();
         $this->db->reset_query();
         return $this->db->query("($tweets) UNION ($retweets) ORDER BY order_time DESC")->result_array();
+    }
+
+    public function createRetweet($tweetId, $userId)
+    {
+        $today = date('Y-m-d H:i:s');
+
+        $data = [
+            'retweet' => TRUE,
+            'updated_at' => $today,
+        ];
+        $this->db->where('id', $tweetId)->update('tweets', $data);
+
+        $data = [
+            'user_id' => $userId,
+            'created_at' => $today,
+            'updated_at' => $today,
+        ];
+        $this->db->insert('retweets', $data);
+        $retweetId = $this->db->insert_id();
+
+        $data = [
+            'retweet_id' => $retweetId,
+            'tweet_id' => $tweetId,
+        ];
+        return $this->db->insert('retweet_tweet', $data);
     }
 }
