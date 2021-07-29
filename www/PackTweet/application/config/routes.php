d2a0eb4945d00344aa307941afcc9526dadb6a39
照井拓朗 <>
2021-07-29 19:13:07 +0900

    リツイート処理のルート作成

--- www/PackTweet/application/config/routes.php
+++ www/PackTweet/application/config/routes.php
@@ -67,3 +67,4 @@ $route['tweets/(:num)/delete']['post'] = 'tweets/delete/$1';
 $route['tweets/(:num)/edit']['get'] = 'tweets/edit/$1';
 $route['tweets/(:num)/edit']['post'] = 'tweets/update/$1';
 $route['tweets/mypage'] = 'tweets/mypage';
+$route['tweets/(:num)/retweet']['post'] = 'tweets/retweet/$1';
