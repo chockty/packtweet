6e55d3f16edecc42b586da3dd423ef5c9589db8e
chockty <75976150+chockty@users.noreply.github.com>
2021-07-31 15:15:45 +0900

    ajax通信の処理を追加（途中まで）

--- www/PackTweet/application/public/js/ajaxFavorite.js
+++ www/PackTweet/application/public/js/ajaxFavorite.js
@@ -1,24 +1,28 @@
 $(function () {
   let favorite = $('#js-favorite-toggle');
-  let favoriteId;
-  let csrf_hash = $("#token").val();
-  let csrf_name = $("#token").attr('name');
+  let csrf_name = favorite.prevAll('input[name="csrf_test_name"]');
+  let tweet_id = favorite.prevAll('input[name="tweet_id"]').val();
 
   favorite.on('click', function (e) {
     e.preventDefault();
-    let $this = $(this);
-    favoriteId = $this.data('tweet_id');
+    // console.log(csrf_name.val());
+    // return console.log(csrf_name.attr('name'))
+    // favoriteId = $this.data('tweet_id');
     let postdata = {
-      id: favoriteId,
+      tweet_id: tweet_id,
     }
-    postdata[csrf_name] = csrf_hash;
+    postdata[csrf_name.attr('name')] = csrf_name.val();
     $.ajax({
             url: 'tweets/favorite',
             type: 'POST',
             data: postdata,
     }).done(function (data) {
-            console.log(data);
-            $this.find('i').toggleClass('far fa-bookmark');
+      let $this = $(this);
+      let csrf_token = $('input[name="csrf_test_name"]');
+      csrf_token.val() = data['csrf_hash'];
+			// return console.log('OK');
+      console.log(csrf_token);
+      $this.find('i').toggleClass('far fa-bookmark');
     })
   });
 });
